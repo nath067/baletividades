be97b733e9d49fcac949532b435f63fe23e8686b
Nathali Garcia <nathaligsantos@gmail.com>
2024-09-30 12:01:39 -0300

    atualizado

--- database/baletividades_db.sql
+++ database/baletividades_db.sql
@@ -2,16 +2,15 @@ CREATE DATABASE baletividade_bd;
 USE baletividade_bd;
 
 CREATE TABLE usuario(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY auto_increment,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     nome_usuario VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    tipo_usuario varchar(255),
     foto_perfil VARCHAR(255)
 );
 
-select * from usuario;
+drop table usuario;
 
 CREATE TABLE categorias(
 	salvos_atividade VARCHAR(255),
@@ -23,9 +22,30 @@ CREATE TABLE categorias(
 
 CREATE TABLE salvos (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(255) NOT NULL,
-    nivel varchar(255),
-    img_atividade varchar(255) not null
+	atividade_id int,
+    id_usuario int
 );
 
-drop table usuario;
+drop table salvos;
+
+CREATE TABLE avaliacao (
+	id int primary key auto_increment,
+    nota_avaliacao varchar(255),
+    qtd_avaliacao varchar(255),
+    id_usuario int,
+    id_atividade int
+);
+
+CREATE TABLE atividade (
+	id int primary key auto_increment,
+    nome varchar(255),
+    nivel int,
+    imagem varchar(255)
+);
+
+drop table atividade;
+select * from atividade;
+select * from salvos;
+select * from usuario;
+
+delete from atividade where id > 0;
