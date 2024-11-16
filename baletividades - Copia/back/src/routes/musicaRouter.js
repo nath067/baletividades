7d37a677b5a74d2e93b6df08872f4ae2ec9181d1
nath067 <141923696+nath067@users.noreply.github.com>
2024-11-16 00:58:23 -0300

    Delete baletividades - Copia directory

--- baletividades - Copia/back/src/routes/musicaRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { postarMusica, getMusicas } = require('../controller/musicaController');
-const router = Router();
-
-router.post('/postar/musica', postarMusica);
-router.get('/get/musica', getMusicas);
-
-module.exports = router;
--- baletividades - Copia/materiais/materiais.js
+++
@@ -1,3 +0,0 @@
-// Recupera o usuário que está logado pelo local storage
-const usuario_logado = JSON.parse(localStorage.getItem('usuario'));
-console.log(usuario_logado)
